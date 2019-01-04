3ec83273fa2a7e97f886e7c60de8e9763e4bbc80
Vikas KM <vikki.km@gmail.com>
2019-01-04 21:36:33 +0530

    Added the functionality

--- MessagePassing/messagePassing.js
+++ MessagePassing/messagePassing.js
@@ -1,2 +1,29 @@
-const text = document.getElementById("text");
-console.log(text);
+//IFFE
+
+(function() {
+  document
+    .getElementById("messageForm")
+    .addEventListener("submit", function(e) {
+      //to prevent the page from loading by default
+      e.preventDefault();
+      //getting the value
+      const message = document.getElementById("messageText");
+      const text = message.value;
+
+      //change the text content to the input received
+
+      if (text.length === 0) {
+        document.getElementById("feedback").style.display = "block";
+        setTimeout(function() {
+          document.getElementById("feedback").style.display = "none";
+        }, 2000);
+      } else {
+        document.getElementById("feedback").style.display = "none";
+        document.getElementById("messageVal").textContent = text;
+        document.getElementById("messageVal").style.textTransform = "uppercase";
+      }
+
+      //making the input empty to receive the new input
+      message.value = "";
+    });
+})();
