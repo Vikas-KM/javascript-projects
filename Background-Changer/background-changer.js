2a07cda906c5264ab34e053894bf3f15c0dd6e74
Vikas KM <vikki.km@gmail.com>
2019-01-06 19:52:47 +0530

    changed class and id names

--- Background-Changer/background-changer.js
+++ Background-Changer/background-changer.js
@@ -2,23 +2,23 @@
 // Math.random function works from 0 to 0.99 never reaches one
 // Math.floor is used to get the whole number except the decimal
 
-const btn = document.getElementById('colorBtn');
+const btn = document.getElementById("change-button");
 console.log(btn);
 const body = document.body;
-let hexValue='#';
-let hexValues = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+let hexValue = "#";
+let hexValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-function changeBackgroundColor(){
-    console.log('button clicked');
-    for(let i = 0; i<6; i++){
-        let random = Math.floor(Math.random()*hexValues.length);
-        //hexvalue initally is string so + works as concat
-        hexValue+=hexValues[random];
-    }
-    console.log(hexValue);
-    body.style.backgroundColor = hexValue;
-    hexValue='#';
+function changeBackgroundColor() {
+  console.log("button clicked");
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexValues.length);
+    //hexvalue initally is string so + works as concat
+    hexValue += hexValues[random];
+  }
+  console.log(hexValue);
+  body.style.backgroundColor = hexValue;
+  hexValue = "#";
 }
 
 //changeBackgroundColor is a callback function hence () are not attached
-btn.addEventListener('click', changeBackgroundColor);
+btn.addEventListener("click", changeBackgroundColor);
